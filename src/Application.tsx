16200f2f6a1d2265a311c15a0ac0a90af1c0aa5f
Jon Edwards <jkedwards@me.com>
2021-08-16 20:50:51 -0700

    replace @xstate/fsm with xstate

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,53 +1,73 @@
 import * as React from 'react';
-import { assign, createMachine, interpret } from '@xstate/fsm';
+import { assign, createMachine, interpret } from 'xstate';
 import Plot from 'react-plotly.js';
 import random from 'random';
 
-const toggleMachine = createMachine(
-	{
-		id: 'toggle',
-		context: {
-			accumulatedData: 0,
-			connected: true,
-		},
-		initial: 'connected',
-		states: {
-			connected: {
-				on: {
-					TOGGLE: 'disconnected',
-					HANDLE_DATA: { actions: ['collectData'] },
+const remoteDeviceFactory = () =>
+	createMachine(
+		{
+			id: 'toggle',
+			context: {
+				accumulatedData: 0,
+				connected: true,
+			},
+			initial: 'idle',
+			states: {
+				idle: {
+					always: 'connected',
+				},
+				connected: {
+					on: {
+						TOGGLE: 'disconnected',
+						HANDLE_DATA: { actions: ['collectData', 'sendData'] },
+					},
+					activities: ['continuouslyCollectData'],
+				},
+				disconnected: {
+					on: {
+						TOGGLE: 'connected',
+						HANDLE_DATA: { actions: ['collectData'] },
+					},
+					activities: ['continuouslyCollectData'],
 				},
 			},
-			disconnected: {
-				on: {
-					TOGGLE: 'connected',
-					HANDLE_DATA: { actions: ['collectData'] },
+		},
+		{
+			actions: {
+				collectData: assign({
+					accumulatedData: (context) => context.accumulatedData + 100,
+				}),
+				sendData: assign({
+					accumulatedData: (context) => context.accumulatedData - 150,
+				}),
+			},
+			activities: {
+				continuouslyCollectData: (context, activity) => {
+					// Start the beeping activity
+					const interval = setInterval(() => {
+						console.log('BEEP!');
+					}, 1000);
+
+					// Return a function that stops the beeping activity
+					return () => clearInterval(interval);
 				},
 			},
 		},
-	},
-	{
-		actions: {
-			collectData: assign({
-				accumulatedData: (context) => context.accumulatedData + 100,
-			}),
-		},
-	},
-);
+	);
 
 const toggleService = [];
 
 const numberOfDevices = 3;
 
 for (let i = 0; i < numberOfDevices; i++) {
-	toggleService[i] = interpret(toggleMachine).start();
+	toggleService[i] = interpret(remoteDeviceFactory()).start();
 
 	toggleService[i].subscribe((state) => {
 		console.log(state);
 	});
 
 	setInterval(() => {
-		if (random.int(0, 100) > 95) {
+		if (random.int(0, 100) > 75) {
 			toggleService[i].send('TOGGLE');
 		}
 		toggleService[i].send('HANDLE_DATA');
